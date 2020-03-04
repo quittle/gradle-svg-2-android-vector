955ba08630b23c9481c428da5d37ce4a57ec99c7
Dustin Toff <me@dustintoff.com>
2020-03-04 00:00:22 +0000

    Fix Gradle warnings
    - Now tasks show up as UP-TO-DATE properly

--- svg-2-android-vector/src/main/java/com/quittle/svg2androidvector/Svg2AndroidVectorTask.java
+++ svg-2-android-vector/src/main/java/com/quittle/svg2androidvector/Svg2AndroidVectorTask.java
@@ -13,6 +13,7 @@ import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskExecutionException;
 
 /**
@@ -24,49 +25,58 @@ public class Svg2AndroidVectorTask extends DefaultTask {
      * The SVG file to convert from
      */
     @InputFile
-    public File svg;
+    public File getSvg() {
+        return svg;
+    }
 
     /**
      * The Android vector xml file to convert to.
      */
     @OutputFile
-    public File xml;
+    public File getXml() {
+        return xml;
+    }
 
     /**
      * Whether or not to fail the task if only part of the SVG could be converted.
      */
     @Input
-    public boolean failOnWarning = true;
+    public boolean getFailOnWarning() {
+        return failOnWarning;
+    }
 
-    public Svg2AndroidVectorTask() {
-        doLast(task -> {
-            try {
-                mkdirs(xml.getParentFile());
-                try (final OutputStream os = new FileOutputStream(xml)) {
-                    // parseSvgToXml does not throw on error, it simply returns a log of what it could not convert and
-                    // writes nothing out if it completely failed.
-                    final String errorLog = Svg2Vector.parseSvgToXml(svg, os);
+    File svg;
+    File xml;
+    boolean failOnWarning = true;
 
-                    // Handle complete error where the SVG could not be converted at all.
-                    os.flush();
-                    if (xml.length() == 0) {
-                        throw new TaskExecutionException(this,
-                                new RuntimeException(
-                                        svg.getAbsolutePath() + " unable to be converted to Android vector: " + errorLog));
-                    }
+    @TaskAction
+    public void action() {
+        try {
+            mkdirs(xml.getParentFile());
+            try (final OutputStream os = new FileOutputStream(xml)) {
+                // parseSvgToXml does not throw on error, it simply returns a log of what it could not convert and
+                // writes nothing out if it completely failed.
+                final String errorLog = Svg2Vector.parseSvgToXml(svg, os);
 
-                    // Handle partial error where not everything could be converted but a drawable was able to be created.
-                    if (failOnWarning && errorLog.length() != 0) {
-                        throw new TaskExecutionException(this, new RuntimeException(errorLog));
-                    }
-                } catch (final IOException e) {
-                    throw new TaskExecutionException(this, new IOException("Unable to write out Android vector file", e));
+                // Handle complete error where the SVG could not be converted at all.
+                os.flush();
+                if (xml.length() == 0) {
+                    throw new TaskExecutionException(this,
+                            new RuntimeException(
+                                    svg.getAbsolutePath() + " unable to be converted to Android vector: " + errorLog));
                 }
-            } catch (final TaskExecutionException e) {
-                delete(xml);
-                throw e;
+
+                // Handle partial error where not everything could be converted but a drawable was able to be created.
+                if (failOnWarning && errorLog.length() != 0) {
+                    throw new TaskExecutionException(this, new RuntimeException(errorLog));
+                }
+            } catch (final IOException e) {
+                throw new TaskExecutionException(this, new IOException("Unable to write out Android vector file", e));
             }
-        });
+        } catch (final TaskExecutionException e) {
+            delete(xml);
+            throw e;
+        }
     }
 
     private static void mkdirs(final File directory) {
