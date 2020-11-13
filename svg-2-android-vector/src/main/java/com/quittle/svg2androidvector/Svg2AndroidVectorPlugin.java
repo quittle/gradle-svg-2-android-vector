f9663fdc4349a6601030bff0b647d242e7f67b42
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-11-13 15:53:23 -0600

    Fix null pointer exception seen when applied to something other than an Android project.
    This is a common problem when applying to a root project with a child, Android project as recommended by
    Android in all their examples.
    
    Co-authored-by: Dustin Toff <me@dustintoff.com>

--- svg-2-android-vector/src/main/java/com/quittle/svg2androidvector/Svg2AndroidVectorPlugin.java
+++ svg-2-android-vector/src/main/java/com/quittle/svg2androidvector/Svg2AndroidVectorPlugin.java
@@ -3,9 +3,11 @@ package com.quittle.svg2androidvector;
 import com.android.build.gradle.BaseExtension;
 import com.android.build.gradle.api.AndroidSourceDirectorySet;
 import com.android.build.gradle.api.AndroidSourceSet;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.TaskContainer;
 
 import java.io.File;
@@ -53,14 +55,24 @@ public class Svg2AndroidVectorPlugin implements Plugin<Project> {
     }
 
     private static void onAfterEvaluate(final Project project, final Svg2AndroidVectorExtension extension) {
+        final Logger logger = project.getLogger();
+        final String displayName = project.getDisplayName();
+        // The base Android extension, where all resources can be found.
+        final BaseExtension androidExtension = project.getExtensions().findByType(BaseExtension.class);
+        if (androidExtension == null) {
+            // This might mean it was applied to a non-Android project, possibly just the root project.
+            logger.info(
+                    "{} had the Svg2AndroidVector plugin applied, " +
+                    "but the android plugin was not detected. Doing nothing",
+                    displayName);
+            return;
+        }
         final TaskContainer taskContainer = project.getTasks();
         final Task parentTask = taskContainer.create(CONVERSION_PARENT_TASK_NAME);
         // An early Android task all the conversion tasks should be a dependency of
         taskContainer.findByName(EARLY_ANDROID_TASK_NAME).dependsOn(parentTask);
         // The folder to put the converted files
         final File generatedResourceDir = new File(project.getBuildDir(), BUILD_DIR_RESOURCE_NAME);
-        // The base Android extension, where all resources can be found.
-        final BaseExtension androidExtension = project.getExtensions().getByType(BaseExtension.class);
         for (final AndroidSourceSet sourceSet : androidExtension.getSourceSets()) {
             final String sourceSetName = sourceSet.getName(); // e.g. main, androidTest, debug, etc.
             final AndroidSourceDirectorySet sourceDirectorySet = sourceSet.getRes();
@@ -70,11 +82,11 @@ public class Svg2AndroidVectorPlugin implements Plugin<Project> {
                     .forEach(svgFile -> {
                         // Create a task to build the Android vector equivalent file in an Android resource
                         // directory specifically for this source set
-                        final File newResouceDir = new File(generatedResourceDir, sourceSetName);
+                        final File newResourceDir = new File(generatedResourceDir, sourceSetName);
                         taskContainer.create(buildTaskName(svgFile), Svg2AndroidVectorTask.class, task -> {
                             task.svg = svgFile;
                             task.xml = Paths.get(
-                                    newResouceDir.getAbsolutePath(),
+                                    newResourceDir.getAbsolutePath(),
                                     ANDROID_RESOURCES_DIR_NAME_DRAWABLE,
                                     svgFile.getName().replace(SVG_FILE_EXTENSION, XML_FILE_EXTENSION)).toFile();
                             task.failOnWarning = extension.getFailOnWarning();
@@ -82,7 +94,7 @@ public class Svg2AndroidVectorPlugin implements Plugin<Project> {
                         });
 
                         // Add the new, generated resource directory for the source set
-                        sourceDirectorySet.srcDir(newResouceDir);
+                        sourceDirectorySet.srcDir(newResourceDir);
                         // Remove the original resource from the resources. Two resources with the same name but
                         // different types may not share the same name.
                         sourceDirectorySet.getFilter().exclude(svgFile.getAbsolutePath());
